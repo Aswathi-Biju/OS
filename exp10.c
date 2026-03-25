4b690b260e18804df9a69b3d92f6e631105de8a6
Aswathi Biju <aswathibiju8118@gmail.com>
2026-03-25 13:57:17 +0530

    Update exp10.c

--- exp10.c
+++ exp10.c
@@ -1,127 +1,127 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-#define s 10
-#define m 5000
+// #include<stdio.h>
+// #include<stdlib.h>
+// #include<time.h>
+// #define s 10
+// #define m 5000
 
-void SSTF(int req[], int head){
-    int visited[s]={0};
-    int total = 0;
-    for(int i=0; i<s; i++){
-        int min=10000, index = -1;
-        for(int j = 0; j<s; j++){
-            if(!visited[j]){
-                int dist = abs(head-req[j]);
-                if(dist<min){
-                    min =  dist;
-                    index = j;
-                }
-            }
-        }
-        visited[index] = 1;
-        total += min;
-        head = req[index];
-    }
-    printf("SSTF Total Seek Time: %d\n", total);
-}
+// void SSTF(int req[], int head){
+//     int visited[s]={0};
+//     int total = 0;
+//     for(int i=0; i<s; i++){
+//         int min=10000, index = -1;
+//         for(int j = 0; j<s; j++){
+//             if(!visited[j]){
+//                 int dist = abs(head-req[j]);
+//                 if(dist<min){
+//                     min =  dist;
+//                     index = j;
+//                 }
+//             }
+//         }
+//         visited[index] = 1;
+//         total += min;
+//         head = req[index];
+//     }
+//     printf("SSTF Total Seek Time: %d\n", total);
+// }
 
-void LOOK(int req[], int head){
-    int total = 0;
-    for (int i=0; i<s-1; i++){
-        for(int j=0; j<s-i-1; j++){
-            if(req[j]>req[j+1]){
-                int temp = req[j];
-                req[j] = req[j+1];
-                req[j+1] = temp;
-            }
-        }
-    }
-    int i;
-    int pos;
-    for(i = 0; i < s; i++){
-        if(req[i] >= head){
-            pos = i;
-            break;
-        }
-    }
+// void LOOK(int req[], int head){
+//     int total = 0;
+//     for (int i=0; i<s-1; i++){
+//         for(int j=0; j<s-i-1; j++){
+//             if(req[j]>req[j+1]){
+//                 int temp = req[j];
+//                 req[j] = req[j+1];
+//                 req[j+1] = temp;
+//             }
+//         }
+//     }
+//     int i;
+//     int pos;
+//     for(i = 0; i < s; i++){
+//         if(req[i] >= head){
+//             pos = i;
+//             break;
+//         }
+//     }
 
-    // move right
-    for(i = pos; i < s; i++){
-        total += abs(head - req[i]);
-        head = req[i];
-    }
+//     // move right
+//     for(i = pos; i < s; i++){
+//         total += abs(head - req[i]);
+//         head = req[i];
+//     }
 
-    // move left
-    for(i = pos - 1; i >= 0; i--){
-        total += abs(head - req[i]);
-        head = req[i];
-    }
-    printf("LOOK Total Seek Time: %d\n", total);
-}
+//     // move left
+//     for(i = pos - 1; i >= 0; i--){
+//         total += abs(head - req[i]);
+//         head = req[i];
+//     }
+//     printf("LOOK Total Seek Time: %d\n", total);
+// }
 
-void CSCAN(int req[], int head){
-    int total = 0;
-    for(int i=0; i<s-1; i++){
-        for(int j=0; j<s-i-1; j++){
-            if(req[j]>req[j+1]){
-                int temp = req[j];
-                req[j] = req[j+1];
-                req[j+1] = temp;
-            }
-        }
-    }
-    int i;
-    int pos;
-    for(i = 0; i < s; i++){
-        if(req[i] >= head){
-            pos = i;
-            break;
-        }
-    }
-    // move right
-    for(i = pos; i < s; i++){
-        total += abs(head - req[i]);
-        head = req[i];
-    }
+// void CSCAN(int req[], int head){
+//     int total = 0;
+//     for(int i=0; i<s-1; i++){
+//         for(int j=0; j<s-i-1; j++){
+//             if(req[j]>req[j+1]){
+//                 int temp = req[j];
+//                 req[j] = req[j+1];
+//                 req[j+1] = temp;
+//             }
+//         }
+//     }
+//     int i;
+//     int pos;
+//     for(i = 0; i < s; i++){
+//         if(req[i] >= head){
+//             pos = i;
+//             break;
+//         }
+//     }
+//     // move right
+//     for(i = pos; i < s; i++){
+//         total += abs(head - req[i]);
+//         head = req[i];
+//     }
 
-    // go to end
-    total += abs(head - (m - 1));
+//     // go to end
+//     total += abs(head - (m - 1));
 
-    // jump to start
-    total += (m - 1);
-    head = 0;
+//     // jump to start
+//     total += (m - 1);
+//     head = 0;
 
-    // service remaining
-    for(i = 0; i < pos; i++){
-        total += abs(head - req[i]);
-        head = req[i];
-    }
-    printf("CSCAN Total Seek Time: %d\n", total);
-}
+//     // service remaining
+//     for(i = 0; i < pos; i++){
+//         total += abs(head - req[i]);
+//         head = req[i];
+//     }
+//     printf("CSCAN Total Seek Time: %d\n", total);
+// }
 
-int main(){
-    int r1[s], r2[s], r3[s], head;
-    srand(time(0));
-    for(int i=0; i<s; i++){
-        r1[i]= rand()%m;
-    }
-    //r1 for SSTF, r2 for LOOK, r3 for CSCAN
-    for(int i=0; i<s; i++){
-        r2[i]=r1[i];
-        r3[i]=r1[i];
-    }
-    printf("Request: ");
-    for(int i=0;i<s; i++){
-        printf("%d ", r1[i]);
-    }
-    printf("\n");
-    head= rand()%m;
-    printf("Initial Head: %d\n", head);
-    SSTF(r1,head);
-    LOOK(r2,head);
-    CSCAN(r3,head);
-    return 0;
-}
+// int main(){
+//     int r1[s], r2[s], r3[s], head;
+//     srand(time(0));
+//     for(int i=0; i<s; i++){
+//         r1[i]= rand()%m;
+//     }
+//     //r1 for SSTF, r2 for LOOK, r3 for CSCAN
+//     for(int i=0; i<s; i++){
+//         r2[i]=r1[i];
+//         r3[i]=r1[i];
+//     }
+//     printf("Request: ");
+//     for(int i=0;i<s; i++){
+//         printf("%d ", r1[i]);
+//     }
+//     printf("\n");
+//     head= rand()%m;
+//     printf("Initial Head: %d\n", head);
+//     SSTF(r1,head);
+//     LOOK(r2,head);
+//     CSCAN(r3,head);
+//     return 0;
+// }
 
 
 
